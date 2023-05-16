703301c327f4fcc991c3e4cee6c3b762c4af76ed
JoaquinCT <jose.cortes.tenorio@est.una.ac.cr>
2023-05-15 23:47:17 -0600

    Create Proveedor.java

---
+++ ProyectoTecSolve/src/main/java/cr/ac/una/tecsolve/domain/Proveedor.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.tecsolve.domain;
+
+/**
+ *
+ * @author kinco
+ */
+public class Proveedor {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String email;
+
+    public Proveedor(String nombre, String direccion, String telefono, String email) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
